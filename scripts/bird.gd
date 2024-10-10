7ff40655c2ba6c8c1eba4f782ab6ed84bcfdedd8
Shawn Taylor <shawn@mapsandapps.net>
2024-10-10 19:59:01 -0400

    Tell bird to move

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -1,17 +1,34 @@
 extends CharacterBody2D
 
-
-#const SPEED = 300.0
 const SPEED = 0
-#const JUMP_VELOCITY = -400.0
 const JUMP_VELOCITY = 0
 
+const POSSIBLE_MOVES = ['hop', 'walk', 'turn']
+
+# any of the possible bird movements
+func move():
+	# TODO: pick a movement
+	var move = POSSIBLE_MOVES.pick_random()
+	print(move)
+
+	# TODO: move bird
+	if move == 'hop':
+		hop()
+	elif move == 'walk':
+		walk()
+	elif move == 'turn':
+		turn()
+
+func hop():
+	print('hop')
+
+func walk():
+	print('walk')
+
+func turn():
+	print('turn')
 
 func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
--- scripts/birds.gd
+++ scripts/birds.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 @export var bird_scene: PackedScene
 
+const MIN_BIRD_TIMING = 0
+const MAX_BIRD_TIMING = 5
+
 var birds: Array = []
 var click_position = Vector2()
+var next_bird_action_timing = randf_range(MIN_BIRD_TIMING, MAX_BIRD_TIMING)
 
 var screenSize: Vector2 = DisplayServer.window_get_size()
 #get_viewport().get_visible_rect().size
@@ -17,19 +21,30 @@ func spawn_bird(pos: Vector2 = get_random_position()):
 	bird.rotation = randf_range(0, 2 * PI)
 	birds.append(bird)
 	add_child(bird)
+	
+# QUESTION: should this be in bird.gd?
+# no, but the move command should be
+func move_random_bird():
+	# TODO: pick a random bird
+	var bird_to_move = birds.pick_random()
+	
+	# TODO: tell that bird to move
+	bird_to_move.move()
+	
+	# set when the next bird will move
+	next_bird_action_timing = randf_range(MIN_BIRD_TIMING, MAX_BIRD_TIMING)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in 10:
 		spawn_bird()
-	#birds = get_children()
-	#for i in birds.size():
-		#birds[i].position = Vector2(randf_range(0, screenSize.x), randf_range(0, screenSize.y))
-		#
-		#birds[i].rotation = randf_range(0, 2 * PI)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("tap"):
 		spawn_bird(get_global_mouse_position())
-		
+
+	next_bird_action_timing -= delta
+
+	if next_bird_action_timing <= 0:
+		move_random_bird()
